fc565a931ed221de6068f32a336a24231bcfa548
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-10-06 08:29:45 +0900

    update icon and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>👨🏼‍💻</text></svg>"
+        ></link>
+      </head>
       <body>{children}</body>
     </html>
   );
