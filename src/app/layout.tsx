8150340ef73e577954678fcd880e6f10815566d6
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-10-21 09:39:06 +0900

    Add apple-touch-icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>👨🏼‍💻</text></svg>"
         ></link>
+        <link rel="apple-touch-icon" href="/src/app/img/me-square.png" sizes="192x192" />
       </head>
       <body>{children}</body>
     </html>
