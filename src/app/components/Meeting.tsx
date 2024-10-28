3488894e7e964d7c8743fb64427246e0adbb919e
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-10-28 10:49:41 +0900

    fix

--- src/app/components/Meeting.tsx
+++ src/app/components/Meeting.tsx
@@ -1,17 +1,34 @@
 "use client";
 import { useSearchParams } from "next/navigation";
+import { useState } from "react";
 import { writeItem } from "../logic/actions";
 
 const Meeting: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   const search = useSearchParams();
   const meet = search.get("meet");
 
-  const sendAction = async () => {
+  const sendAction = async (event: React.FormEvent) => {
+    
     try {
-      const items = await writeItem();
-      console.log(`items=${JSON.stringify(items)}`);
+      event.preventDefault();
+      const form = event.target as HTMLFormElement;
+      const formData = new FormData(form); // FormDataを生成
+      
+      document.querySelector("#submitButton")?.setAttribute("disabled", "");
+      await writeItem(formData, meet);
     } catch (e: unknown) {
-      console.error(`error=${e}`);
+      console.error("Failed to write item.", e);
+      alert("出会った記録の書き込みに失敗しました");
+    } finally {
+      closeModal();
     }
   };
 
@@ -21,10 +38,42 @@ const Meeting: React.FC = () => {
         <div className="text-center">
           <button
             className="font-bold rounded-lg px-5 py-2 mb-4 w-auto text-slate-200 bg-purple-700 dark:bg-purple-800 hover:opacity-50"
-            onClick={() => sendAction()}
+            onClick={openModal}
           >
             Today we&#39;ve met!
           </button>
+        </div>
+      )}
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-slate-400 p-4 rounded-lg text-white w-3/4">
+            <form onSubmit={sendAction}>
+              <p>
+                Toru Takahashiと今日出会ったことを記録します<br />
+              </p>
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter your name"
+                className="border border-gray-300 rounded-lg px-3 py-2 w-full text-slate-800 bg-white"
+              />
+              <div className="text-slate-300 text-sm mb-2">お名前を入力してください。ニックネーム・ハンドルネーム等も可。</div>
+              <div className="flex justify-end">
+                <button
+                  onClick={closeModal}
+                  className="bg-gray-300 hover:bg-gray-400 text-white font-bold py-2 px-4 rounded-lg mr-2"
+                >
+                  Cancel
+                </button>
+                <input
+                  type="submit"
+                  id="submitButton"
+                  className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg cursor-pointer"
+                  value="Send"
+                />
+              </div>
+            </form>
+          </div>
         </div>
       )}
     </>
--- src/app/logic/actions.ts
+++ src/app/logic/actions.ts
@@ -4,14 +4,48 @@ import type { Schema } from "@/../amplify/data/resource";
 import outputs from "@/../amplify_outputs.json";
 import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
+import z from "zod";
+import { CreateItemException } from "../exception/exceptions";
 
 Amplify.configure(outputs);
 const client = generateClient<Schema>();
+const MEET_TOKEN = process.env.MEET_TOKEN;
 
-export async function writeItem() {
-  const { data: items, errors } = await client.models.Meeting.list();
-  if (errors) {
-    throw errors;
+const schema = z.object({
+  name: z
+    .string({
+      required_error: "Name is required.",
+    })
+    .min(1, "At least 1 character is required.")
+    .trim(),
+});
+
+export async function writeItem(formData: FormData, token: string | null) {
+  if (!isValidToken(token)) {
+    console.error("Token error.", token);
+    throw new CreateItemException("Failed to token validation.");
   }
-  return items;
+  validateForm(formData);
+
+  createItem(formData.get("name") as string);
 }
+
+const validateForm = (formData: FormData) => {
+  const parse = schema.safeParse({
+    name: formData.get("name"),
+  });
+  if (!parse.success) {
+    console.log("parse error", parse.error);
+    throw new CreateItemException("Validation error.");
+  }
+};
+
+const isValidToken = (token: string | null): boolean => {
+  return token === MEET_TOKEN;
+};
+
+const createItem = async (name: string) => {
+  await client.models.Meeting.create({
+    name: name,
+  });
+};
