e6e973e1f3744c9eaebe0125f8954a75eb934ad2
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-10-29 15:09:10 +0900

    lg

--- src/app/logic/action.ts
+++ src/app/logic/action.ts
@@ -9,7 +9,7 @@ const MEET_TOKEN = process.env.MEET_TOKEN;
 
 export async function writeItem(formData: FormData, token: string | null) {
   if (token !== MEET_TOKEN) {
-    console.error("Token error.", token);
+    console.error(`Token error. token=${token} MEET_TOKEN=${MEET_TOKEN}`);
     throw new CreateItemException("Failed to token validation.");
   }
   validateForm(formData);
